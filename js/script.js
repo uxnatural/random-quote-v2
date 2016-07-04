e2c4cb9922c069fbb72833f28186ea767fbb6659
uxnatural <todd@uxnatural.com>
2016-07-03 20:03:41 -0600

    added comments and functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,45 @@
-/* Check if jQuery was initialized and if not (CDN was down for example), then
-*/ load jQuery from a local source in the js folder of the project.
+/* 
+Check if jQuery was initialized and if not (CDN was down for example), then
+load jQuery from a local source in the js folder of the project.
+*/
+if (window.jQuery) {  
+    console.log("You are running jQuery version: " + $.fn.jquery);  
+} else {
+    console.log("");
+}
+
 if(typeof jQuery === 'undefined'){
     document.write(unescape("%3Cscript src='js/jquery-3.0.0.min.js' type='text/javascript'%3E%3C/script%3E"));
+}
+
+/*
+On documentReady get the quotes using an ajax request
+https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+*/
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+function loadJSON(callback) {   
+
+var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+xobj.open('GET', 'js/quotes.json', true);
+xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
+      }
+};
+xobj.send(null);  
+}
+
+function init() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+ });
 }
 
 
@@ -13,11 +51,10 @@ Create a function named getRandomQuote which:
 
 
 function getRandomQuote (){
-
 };
 
 // event listener to respond to "Show another quote" button clicks
-function printQuote();
+
 
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
@@ -29,4 +66,5 @@ Create a function named printQuote which follows these rules:
 3. printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
 4. printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
 */
+function printQuote(){}
 
