cc7e9126061ed5dd5b0ae0abb4f7ac03c3217419
uxnatural <todd@uxnatural.com>
2016-12-04 20:30:23 -0700

    Added extra credit code
    added code blocks addressing the extra credit activity "Don't display a
    random quote more than once until ALL quotes from the array have been
    displayed."

--- js/script.js
+++ js/script.js
@@ -25,15 +25,24 @@ var quotes =
 		"year": 1990,
 		"tags": "Philosophy",
 		"amazon": "<iframe type=\"text/html\" width=\"212\" height=\"362\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002RI945U&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_5JRdyb1C1XC8G\" ></iframe>"
-	}]
+	}];
 
-
+//global variable to hold a count of array items in quotes
+var quotesCounter = [];
+//count the array items and add each as a number in quotesCounter
+for (var i = 0; i < quotes.length; ++i){
+    quotesCounter.push(i);
+}
 
 function getRandomQuote(){
-   
-    //Selects a random quote object from the quotes array
-    var randomNumber = Math.floor(Math.random() * quotes.length);
-
+    //take a random number from quotesCounter
+    var randomNumber = quotesCounter.splice(Math.random()*quotesCounter.length,1)[0];
+    
+    //disabled the button when the quotesCounter has 1 item left
+    if(quotesCounter.length == 1){
+        document.getElementById('loadQuote').innerHTML = "Sorry, we're out of quotes."
+        document.getElementById('loadQuote').disabled = true;
+    }
     //returns the randomly selected quote object 
     return quotes[randomNumber];
 
@@ -48,16 +57,8 @@ function printQuote(){
     var quoteTemplate = '<p class="quote">' + randomQuote.quote + '</p><p class="source">'+ randomQuote.source + '<span class="citation">' + randomQuote.citation + '</span><span class="year">' + randomQuote.year + '</span> </p><p class="amazon">' + randomQuote.amazon + '</p>';
 
     //printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
-    if (randomQuote.citation == 'undefined') {
-        var elem = document.getElementById("citation");
-        elem.remove();
-    };
-    if (randomQuote.year == 'undefined') {
-        var elem = document.getElementById("year");
-        elem.remove();
-    };
+
     //printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
-    
     document.getElementById('quote-box').innerHTML = quoteTemplate;
 }
 
