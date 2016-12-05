bc8002b0b87e6e56e99231e46e41447836c8ede2
uxnatural <todd@uxnatural.com>
2016-12-04 18:56:52 -0700

    Continued Project
    added new solutions to the treehouse functions and removed old stuff

--- js/script.js
+++ js/script.js
@@ -1,40 +1,7 @@
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-//Using polyfill for ECMA6 .includes
-if (!Array.prototype.includes) {
-  Array.prototype.includes = function(searchElement /*, fromIndex*/) {
-    'use strict';
-    if (this == null) {
-      throw new TypeError('Array.prototype.includes called on null or undefined');
-    }
-
-    var O = Object(this);
-    var len = parseInt(O.length, 10) || 0;
-    if (len === 0) {
-      return false;
-    }
-    var n = parseInt(arguments[1], 10) || 0;
-    var k;
-    if (n >= 0) {
-      k = n;
-    } else {
-      k = len + n;
-      if (k < 0) {k = 0;}
-    }
-    var currentElement;
-    while (k < len) {
-      currentElement = O[k];
-      if (searchElement === currentElement ||
-         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
-        return true;
-      }
-      k++;
-    }
-    return false;
-  };
-}
 
 var quotes =
 	[{
@@ -43,47 +10,54 @@ var quotes =
 		"citation": "The 4-Hour Workweek",
 		"year": 2009,
 		"tags": "Time Management",
-		"amazon": "<iframe type=\"text/html\" width=\"336\" height=\"550\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002WE46UW&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_LCRdybVQ46XNT\" ></iframe>"
+		"amazon": "<iframe type=\"text/html\" width=\"212\" height=\"362\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002WE46UW&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_LCRdybVQ46XNT\" ></iframe>"
 	}, {
 		"quote": "If you tell the truth, you don't have to remember anything.",
 		"source": "Mark Twain",
 		"citation": "p. 240 Mark Twain's Notebook",
 		"year": 1935,
 		"tags": "Essays and Correspondence",
-		"amazon": "<iframe type=\"text/html\" width=\"336\" height=\"550\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B00O869NMS&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_6GRdybFWTZR8G\" ></iframe>"
+		"amazon": "<iframe type=\"text/html\" width=\"212\" height=\"362\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B00O869NMS&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_6GRdybFWTZR8G\" ></iframe>"
 	}, {
 		"quote": "Without music, life would be a mistake.",
 		"source": "Friedrich Nietzsche",
 		"citation": "Twilight of the Idols",
 		"year": 1990,
 		"tags": "Philosophy",
-		"amazon": "<iframe type=\"text/html\" width=\"336\" height=\"550\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002RI945U&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_5JRdyb1C1XC8G\" ></iframe>"
+		"amazon": "<iframe type=\"text/html\" width=\"212\" height=\"362\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002RI945U&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_5JRdyb1C1XC8G\" ></iframe>"
 	}]
 
 
 
 function getRandomQuote(){
-
-    //Create a Random Number Video on Treehouse "https://teamtreehouse.com/library/javascript-basics/working-with-numbers/create-a-random-number"
+   
+    //Selects a random quote object from the quotes array
     var randomNumber = Math.floor(Math.random() * quotes.length);
-    //* EXTRA * Don't display a random quote more than once until ALL quotes from the array have been displayed. 
-    var oldQuote = [];
-    oldQuote.push(randomNumber);    
 
-    //Select a random quote from the quotes array   
-    var randomQuote = quotes[randomNumber];
-
-    
-    //Object Literal Video on Treehouse "https://teamtreehouse.com/library/javascript-loops-arrays-and-objects/tracking-data-using-objects/the-object-literal"
-    //if the value of randomNumber
-    if (oldQuote.includes(randomNumber)){
-        
-    } else {
-        return randomQuote.quote; 
-    }
+    //returns the randomly selected quote object 
+    return quotes[randomNumber];
 
 //??http://stackoverflow.com/questions/15192614/javascript-how-to-stop-a-random-number-from-appearing-twice
 //return the randomly selected quote object
 }
 
+function printQuote(){
+    
+    var randomQuote = getRandomQuote();
+    
+    var quoteTemplate = '<p class="quote">' + randomQuote.quote + '</p><p class="source">'+ randomQuote.source + '<span class="citation">' + randomQuote.citation + '</span><span class="year">' + randomQuote.year + '</span> </p><p class="amazon">' + randomQuote.amazon + '</p>';
+
+    //printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
+    if (randomQuote.citation == 'undefined') {
+        var elem = document.getElementById("citation");
+        elem.remove();
+    };
+    if (randomQuote.year == 'undefined') {
+        var elem = document.getElementById("year");
+        elem.remove();
+    };
+    //printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
+    
+    document.getElementById('quote-box').innerHTML = quoteTemplate;
+}
 
