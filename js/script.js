8205d1d2fef4782d57836bf9946edd164187847c
uxnatural <todd@uxnatural.com>
2017-01-03 17:50:31 -0700

    Updating Project
    Reviewing Project Requirements, getting feedback, and extra credit
    before submitting.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// an array of JavaScript objects to hold the data for quotes.
+// an array of javaScript objects for quotes
 var quotes =
 	[{
 		"quote": "You can do anything but not everything",
@@ -10,7 +10,6 @@ var quotes =
 	}, {
 		"quote": "To enjoy life, you don't need fancy nonsense, but you do need to control your time and realize that most things just aren't as serious as you make them out to be.",
 		"source": "Tim Ferris",
-		"year": 2009,
 		"tags": "Time Management",
 		"amazon": "<iframe type=\"text/html\" width=\"212\" height=\"362\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002WE46UW&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_LCRdybVQ46XNT\" ></iframe>"
 	}, {
@@ -29,9 +28,9 @@ var quotes =
 		"amazon": "<iframe type=\"text/html\" width=\"212\" height=\"362\" frameborder=\"0\" allowfullscreen style=\"max-width:100%\" src=\"https://read.amazon.com/kp/card?asin=B002RI945U&preview=inline&linkCode=kpe&ref_=cm_sw_r_kb_dp_5JRdyb1C1XC8G\" ></iframe>"
 	}];
 
-// empty array to hold a running count of the quotes to display
+// empty array to hold a running count of quotes to display
 var quotesCounter = [];
-// count the array items and add each as a number into quotesCounter
+// count the quotes array items and add each quote as a number into quotesCounter
 for (var i = 0; i < quotes.length; ++i){
     quotesCounter.push(i);
 }
@@ -39,7 +38,7 @@ for (var i = 0; i < quotes.length; ++i){
 function getRandomQuote(){
     // reset the quotesCounter after showing all quotes
     if(quotesCounter.length < 1){
-        alert("the end")
+        //alert("the end")
         for (var i = 0; i < quotes.length; ++i){
             quotesCounter.push(i);
         }
@@ -47,7 +46,7 @@ function getRandomQuote(){
     
     // take a random number from quotesCounter and remove then remove it from the count
     var randomNumber = quotesCounter.splice(Math.random()*quotesCounter.length,1)[0];
-    alert(randomNumber);
+    //alert(randomNumber);
     // return the randomly selected quote object 
     return quotes[randomNumber];
 
@@ -57,14 +56,35 @@ function printQuote(){
     var randomQuote = getRandomQuote();
     var quoteTemplate = '<p class="quote">' + randomQuote.quote + '</p><p class="source">'+ randomQuote.source + '<span class="citation">' + randomQuote.citation + '</span><span class="year">' + randomQuote.year + '</span> </p><p class="amazon">' + randomQuote.amazon + '</p>';
 
-    //printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
-    if (randomQuote.citation == undefined) {
-        $("citation").remove();
-    }
-
     //printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
     document.getElementById('quote-box').innerHTML = quoteTemplate;
+    
+    //printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
+    if (randomQuote.citation === undefined) {
+        $( ".citation" ).remove();
+    }
+    if (randomQuote.year === undefined) {
+        $( ".year" ).remove();
+    }
+    window.setTimeout(printQuote, 30000);
+    //set 30 second timer onscreen
+    function inPageTimer(){
+        var count=30;
+        var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+        function timer()
+        {
+          count=count-1;
+          if (count <= 0)
+          {
+             clearInterval(counter);
+             return;
+          }
+         document.getElementById("timer").innerHTML=count; // watch for spelling
+        }  
+    };
+    inPageTimer();
 }
+
 
 
 // call printQuote() when the page first loads
